27b4e201d71b45e204e42b2b70d9e0766065cfc4
Mstoffberg023 <mstoffberg023@student.wethinkcode.co.za>
2024-11-20 17:28:54 +0200

    added test for edit order

--- serverlessWorkshop/orders/tests/integration/test_api.py
+++ serverlessWorkshop/orders/tests/integration/test_api.py
@@ -64,14 +64,56 @@ def test_add_new_order(global_config, orders_endpoint, user_token):
   assert order_info['status'] == "PLACED"
 
 def test_get_order(global_config, orders_endpoint, user_token):
-  response = requests.get(orders_endpoint + "/" + global_config['orderId'],
-      headers={'Authorization': user_token, 'Content-Type': 'application/json'}
-      )
+    response = requests.get(orders_endpoint + "/" + global_config['orderId'],
+        headers={'Authorization': user_token, 'Content-Type': 'application/json'}
+        )
 
-  logger.debug(response.text)
-  order_info = json.loads(response.text)
-  assert order_info['orderId'] == global_config['orderId']
-  assert order_info['status'] == "PLACED"
-  assert order_info['totalAmount'] == 19.97
-  assert order_info['restaurantId'] == 1
-  assert len(order_info['orderItems']) == 2
+    logger.debug(response.text)
+    order_info = json.loads(response.text)
+    assert order_info['orderId'] == global_config['orderId']
+    assert order_info['status'] == "PLACED"
+    assert order_info['totalAmount'] == 19.97
+    assert order_info['restaurantId'] == 1
+    assert len(order_info['orderItems']) == 2
+
+def test_edit_order(global_config, orders_endpoint, user_token):
+    print(f"Modifying order {global_config['orderId']}")
+
+    modified_order = {
+        "restaurantId": 1,
+        "orderItems": [
+            {
+                "id": 1,
+                "name": "Spaghetti",
+                "price": 9.99,
+                "quantity": 1
+            },
+            {
+                "id": 2,
+                "name": "Pizza - SMALL",
+                "price": 4.99,
+                "quantity": 1
+            },
+            {
+                "id": 3,
+                "name": "Salad - LARGE",
+                "price": 9.99,
+                "quantity": 1
+            },
+        ],
+        "totalAmount": 25.97,
+        "status": "PLACED",
+        "orderTime": global_config['orderTime'],
+    }
+
+    response = requests.put(
+        orders_endpoint + "/" + global_config['orderId'],
+        data=json.dumps(modified_order),
+        headers={'Authorization': user_token, 'Content-Type': 'application/json'}
+        )
+
+    logger.debug(f'Modify order response: {response.text}')
+    assert response.status_code == 200
+    updated_order = response.json()
+    assert updated_order['totalAmount'] == 25.97
+    assert len(updated_order['orderItems']) == 3
